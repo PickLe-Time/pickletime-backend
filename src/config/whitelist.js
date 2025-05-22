9ff5a5ec7206790f0caa3c9f4828a2247515f68c
Kris Van <kristophervan1999@gmail.com>
2025-05-22 18:37:32 -0400

    Added HTTP and HTTPS domains to CORS whitelist

--- src/config/whitelist.js
+++ src/config/whitelist.js
@@ -1,6 +1,7 @@
 // Allowed origins
 export const whitelist = [
-  process.env.DOMAIN_NAME,
+  process.env.DOMAIN_NAME_HTTP,
+  process.env.DOMAIN_NAME_HTTPS,
   'http://0.0.0.0:5000',
   'http://127.0.0.1:5000',
   'http://localhost:5173',
